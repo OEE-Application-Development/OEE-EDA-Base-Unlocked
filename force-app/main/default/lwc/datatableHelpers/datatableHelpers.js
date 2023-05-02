72a8c5b7a4ecb843ab907d50cb575fe63d11bea7
winterrk <winterrk@colostate.edu>
2023-05-01 19:11:07 -0600

    Account Request added.

--- force-app/main/default/lwc/datatableHelpers/datatableHelpers.js
+++ force-app/main/default/lwc/datatableHelpers/datatableHelpers.js
@@ -5,9 +5,11 @@ const parseFieldData = (cols, data) => {
         var record = {'id': recordData.id};
         for(var j=0;j<cols.length;j++) {
             if(cols[j].type == 'button')continue;
+            
             var fields = cols[j].fieldName.split('.');
             var value = (recordData.fields[fields[0]])?recordData.fields[fields[0]].value:'';
             for(var k=1;k<fields.length;k++) {
+                if(value == null) continue;
                 value = (value.fields[fields[k]])?value.fields[fields[k]].value:'';
             }
             record[cols[j].fieldName] = value;
