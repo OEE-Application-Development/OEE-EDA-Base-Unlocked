2968757e4c634ce655a70e07c890ad6ac73a9053
winterrk <winterrk@colostate.edu>
2023-06-14 20:53:31 -0600

    Update workspaceAPI.js

--- force-app/main/default/lwc/workspaceAPI/workspaceAPI.js
+++ force-app/main/default/lwc/workspaceAPI/workspaceAPI.js
@@ -1,50 +1,46 @@
-import { LightningElement } from 'lwc';
-
-export default class WorkspaceAPI extends LightningElement {
-
-    closeCurrentTab() {
-        this.getFocusedTab().then((tab) => {
-            this.invokeWorkspaceAPI('closeTab', {
-                tabId: tab.tabId
-            });
-        })
-    }
-
-    refreshCurrentTab(includeAllSubtabs) {
-        this.getFocusedTab().then((tab) => {
-            this.invokeWorkspaceAPI('refreshTab', {
-                tabId: tab.tabId,
-                includeAllSubtabs: (includeAllSubtabs)?includeAllSubtabs:true
-            })
-        })
-    }
-
-    getFocusedTab() {
-        return this.invokeWorkspaceAPI('getFocusedTabInfo');
-    }
-
-    invokeWorkspaceAPI(method, args) {
-      return new Promise((resolve, reject) => {
-        const apiEvent = new CustomEvent("internalapievent", {
-          bubbles: true,
-          composed: true,
-          cancelable: false,
-          detail: {
-            category: "workspaceAPI",
-            methodName: method,//"getFocusedTabInfo",
-            methodArgs: args,
-            callback: (err, response) => {
-              if (err) {
-                  return reject(err);
-              } else {
-                  return resolve(response);
-              }
-            }
+const invokeWorkspaceAPI = (method, args) => {
+  return new Promise((resolve, reject) => {
+    const apiEvent = new CustomEvent("internalapievent", {
+      bubbles: true,
+      composed: true,
+      cancelable: false,
+      detail: {
+        category: "workspaceAPI",
+        methodName: method,
+        methodArgs: args,
+        callback: (err, response) => {
+          if (err) {
+              return reject(err);
+          } else {
+              return resolve(response);
           }
-        });
-   
-        window.dispatchEvent(apiEvent);
-      });
-    }
+        }
+      }
+    });
 
+    window.dispatchEvent(apiEvent);
+  });
+};
+
+const getFocusedTab = () => {
+  return invokeWorkspaceAPI('getFocusedTabInfo');
 }
+
+const closeCurrentTab = () => {
+  getFocusedTab().then((tab) => {
+      invokeWorkspaceAPI('closeTab', {
+          tabId: tab.tabId
+      });
+  })
+};
+
+const refreshCurrentTab = (includeAllSubtabs) => {
+  getFocusedTab().then((tab) => {
+      invokeWorkspaceAPI('refreshTab', {
+          tabId: tab.tabId,
+          includeAllSubtabs: (includeAllSubtabs)?includeAllSubtabs:true
+      })
+  })
+}
+
+export {invokeWorkspaceAPI, getFocusedTab, closeCurrentTab, refreshCurrentTab}
