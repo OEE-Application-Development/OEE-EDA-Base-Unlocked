355bfdc9e069ecc9c439dffc34b53ecb8b5fcaa1
Kyle Winterrowd <winterrk@colostate.edu>
2024-04-30 17:09:21 -0600

    File upload and link complete

--- force-app/main/default/lwc/objectAttachedImage/objectAttachedImage.js
+++ force-app/main/default/lwc/objectAttachedImage/objectAttachedImage.js
@@ -4,6 +4,7 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 import refreshData from '@salesforce/apex/CombinedFunctions.refreshData';
 import associateFile from '@salesforce/apex/CombinedFunctions.associateFile';
+import getImage from '@salesforce/apex/CombinedFunctions.getImage';
 
 const INVALID_FIELD = new ShowToastEvent({title: 'Field Not Found', message: 'Please edit the Lightning Component - could not find the field specified by Field Field Dev Name.', variant: 'error'});
 const UPLOAD_COMPLETE = new ShowToastEvent({title: 'Upload Complete', message: 'Upload complete - associating with record...', variant: 'success'});
@@ -13,6 +14,14 @@ export default class ObjectAttachedImage extends LightningElement {
     @api fileField;
 
     objectType;
+    imgUrl = null;
+    publicUrl = null;
+    get imgSetClass() {
+        return (this.imgUrl==null)?'hidden':'';
+    }
+
+    contentDocumentId = null;
+
     @api recordId;
     @wire(getRecord, { recordId: '$recordId', fields: ['*.Id'] })
     getObject({error, data}) {
@@ -21,6 +30,19 @@ export default class ObjectAttachedImage extends LightningElement {
             refreshData({ids: [this.recordId], objectName: data.apiName, fieldsToRefresh: [this.fileField]})
                 .then((result) => {
                     // Guaranteed a row 0
+                    console.log(result[0][this.fileField]);
+                    if(result[0][this.fileField]) {
+                        // One more time...
+                        this.contentDocumentId = result[0][this.fileField];
+                        getImage({contentDocumentId: result[0][this.fileField]})
+                            .then((result) => {
+                                this.imgUrl = result.InternalUrl;
+                                this.publicUrl = result.ExternalUrl;
+                            })
+                            .catch((error) => {
+                                console.log(error);
+                            });
+                    }
                     this.token = result[0][this.fileField];
                 })
                 .catch((error) => {
@@ -55,7 +77,20 @@ export default class ObjectAttachedImage extends LightningElement {
 
             let f1 = event.detail.files[0];
             associateFile({recordId: this.recordId, contentDocumentId: f1.documentId, objectName: this.objectType, fieldName: this.fileField})
-                .then(() => {this.dispatchEvent(FILE_SET_COMPLETE);})
+                .then(() => {
+                    this.dispatchEvent(FILE_SET_COMPLETE);
+                
+                    // Finally, refresh the image data
+                    this.contentDocumentId = f1.documentId;
+                    getImage({contentDocumentId: f1.documentId})
+                        .then((result) => {
+                            this.imgUrl = result.InternalUrl;
+                            this.publicUrl = result.ExternalUrl;
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        });
+                })
                 .catch((error) => {console.log(error);});
         }
     }
