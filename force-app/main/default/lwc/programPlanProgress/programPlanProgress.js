c77206cab04f44f149b9fc8632026158890e9df8
Kyle Winterrowd <winterrk@colostate.edu>
2024-06-04 11:10:35 -0600

    v0.8 of program plan checking

--- force-app/main/default/lwc/programPlanProgress/programPlanProgress.js
+++ force-app/main/default/lwc/programPlanProgress/programPlanProgress.js
@@ -1,7 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+
+import getPlanCompletion from '@salesforce/apex/QueryProgramCompletion.getPlanCompletion';
 
 export default class ProgramPlanProgress extends LightningElement {
 
-    
+    @api contactId;
+    @api programPlanId;// Program Plan ID, not AccountId
+
+    @wire(getPlanCompletion, {contactId: '$contactId', programPlanId: '$programPlanId'})
+    displayCompletionData({error, data}) {
+        console.log(data);
+    }
 
 }
