1ffcfc3e738d16679fe99b1ed5aed16516ea2b5b
Kyle Winterrowd <winterrk@colostate.edu>
2024-07-02 12:31:36 -0600

    Program Plan progress added

--- force-app/main/default/lwc/programPlanButtons/programPlanButtons.js
+++ force-app/main/default/lwc/programPlanButtons/programPlanButtons.js
@@ -3,8 +3,10 @@ import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 import isValidCreditPlan from '@salesforce/apex/QueryProgramCompletion.isValidCreditPlan';
+import getPlanCompletion from '@salesforce/apex/QueryProgramCompletion.getPlanCompletion';
 
 const CLEAN_PROGRAM = new ShowToastEvent({title: 'Program Plan Validity', message: 'Program Plan appears valid.', variant: 'success'});
+const NO_CONTACT = new ShowToastEvent({title: 'Program Progess', message: 'No Contact Chosen.', variant: 'error'});
 export default class ProgramPlanButtons extends LightningElement {
 
     @api recordId;
@@ -18,9 +20,33 @@ export default class ProgramPlanButtons extends LightningElement {
 
             this.dispatchEvent(new ShowToastEvent({
                 title: 'Program Plan Validity',
-                message: result.join('<br />'),
-                variant: 'error'
+                message: result.join("\r\n"),
+                variant: 'error',
+                mode: 'sticky'
             }));
+        });
+    }
+
+    showStudent(e) {
+        this.showContactCard = !this.showContactCard;
+    }
+
+    showContactCard = false;
+    get studentProgressLabel() {
+        return (this.showContactCard)?"Close Student Progress":"Open Student Progress";
+    }
+    get contactCardClass() {
+        return (this.showContactCard)?"":"hidden";
+    }
+
+    checkStudent(event) {
+        if(event.detail.contacts.length == 0 || event.detail.contacts[0] == "fakeid") {
+            this.dispatchEvent(NO_CONTACT);
+            return;
+        }
+        getPlanCompletion({contactId: event.detail.contacts[0], programPlanId: this.recordId}).then(result => {
+            console.log(result);
+            this.dispatchEvent(new ShowToastEvent({title: 'Student Plan Progress', message: 'Status: {0} / Required Credits: {1} / Earned Credits: {2}', messageData: [result.Result.Status, result.Result.TotalCredits, result.Result.EarnedCredits]}))
         });
     }
 
